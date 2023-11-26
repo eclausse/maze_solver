6c46e10f31e612e9fa078bac15f82bf9db4dabd1
Xess1407 <evanclausse@yahoo.fr>
2023-11-26 14:56:08 +0100

    basic Node Maze

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,67 @@
+use std::collections::HashSet;
+
+#[derive(Eq, Hash, PartialEq, Clone)]
+enum Direction {
+    SOUTH, NORTH, WEST, EAST
+}
+
+#[derive(Clone)]
+struct Node {
+    access: HashSet<Direction>,
+    visited: bool,
+    start: bool,
+    end: bool,
+}
+
+impl Node {
+    pub fn new() -> Self {
+        Node { access: HashSet::new(), visited: false, start: false, end: false }
+    }
+}
+
+struct Maze {
+    cell: Vec<Vec<Node>>,
+}
+
+impl Maze {
+    pub fn new(width: usize, height: usize) -> Self {
+        Maze { cell: vec![vec![Node::new(); width]; height] }
+    }
+
+    pub fn print(&self) {
+        for row in self.cell.iter() {
+            for column in row.iter() {
+                print!("+{}", if column.access.contains(&Direction::NORTH) {"   "} else {"---"});
+            }
+            print!("+");
+            println!();
+            for column in row.iter() {
+                print!("{}   ", if column.access.contains(&Direction::WEST) {" "} else {"|"});
+            }
+            print!("|");
+            println!();
+            for column in row.iter() {
+                print!("{0}{1}", if column.access.contains(&Direction::WEST) {" "} else {"|"},
+                                 if column.visited {" . "} else {"   "});
+            }
+            print!("|");
+            println!();
+            for column in row.iter() {
+                print!("{}   ", if column.access.contains(&Direction::WEST) {" "} else {"|"});
+            }
+            print!("|");
+            println!();
+        }
+
+        for _ in self.cell[0].iter() {
+            print!("+---");
+        }
+        print!("+");
+        println!();
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let maze: Maze = Maze::new(5, 5);
+    maze.print();
 }
